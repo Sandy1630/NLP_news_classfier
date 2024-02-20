e0dbd55c4fb69d2c6c8dcc72ec491ce8d3aee402
sandy1630 <santhoshkumar.e2000@gmail.com>
2024-02-20 21:14:06 +0530

    update pickle

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import make_pipeline
+from sklearn.svm import SVC
+import pickle
+
+
+
+
+with open("svm_model_news.pkl","rb") as file:
+    model=pickle.load(file)
+with open("vectorizer_model.pkl","rb") as f:
+    tfid_vector=pickle.load(f)
